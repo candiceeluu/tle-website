fdb720276f48e47c5a08854a894a7c1bf34dc066
candice lu <candicelu@candices-MBP.attlocal.net>
2026-03-03 01:37:48 -0800

    NEW: Added banner

--- src/layout.jsx
+++ src/layout.jsx
@@ -1,11 +1,14 @@
 import Header from './components/layout/Header';
 import Footer from './components/layout/Footer';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
+import Banner from './components/layout/Banner';
 
 export default function Layout() {
+    const location = useLocation();
     return (
     <>
         <Header />
+        {location.pathname === '/' && <Banner />}
         <Outlet />
         <Footer />
     </>
